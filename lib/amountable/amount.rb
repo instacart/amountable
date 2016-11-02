e034b60cfc9363ce0a31ccc775e9abed28750f7b
Emmanuel Turlay <emmanuel@instacart.com>
2016-11-02 16:19:28 -0700

    Enable Amountable::Amount to exist without a database table

--- lib/amountable/amount.rb
+++ lib/amountable/amount.rb
@@ -2,12 +2,25 @@
 
 module Amountable
   class Amount < ActiveRecord::Base
+    class_attribute :columns
+    self.columns = []
+
+    def self.column(name, sql_type = nil, default = nil, null = true)
+      type = "ActiveRecord::Type::#{sql_type.to_s.camelize}".constantize.new
+      columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, type, null)
+    end
+
+    column :amountable_type, :string, nil, false
+    column :amountable_id, :integer, nil, false
+    column :value_currency, :string, nil, true
+    column :value_cents, :integer, 0, false
+    column :name, :string, nil, false
 
     include Amountable::Operations
 
     belongs_to :amountable, polymorphic: true
 
-    monetize :value_cents
+    monetize :value_cents, with_model_currency: :value_currency
 
     validates :name, presence: true
     validates :name, uniqueness: {scope: [:amountable_id, :amountable_type]}
